7acbdbc539299fbb4a773fad4b1a3e6168426c03
pomdtr <achille@deta.sh>
2023-05-20 13:04:21 +0200

    update extension icon, remove option page

--- extension/src/manifest.ts
+++ extension/src/manifest.ts
@@ -8,10 +8,7 @@ export const manifest: chrome.runtime.ManifestV3 = {
   manifest_version: 3,
   action: {
     default_icon: {
-      16: "icons/16.png",
-      19: "icons/19.png",
-      32: "icons/32.png",
-      38: "icons/38.png",
+      48: "icons/48.png",
     },
     default_popup: "src/entries/popup/index.html",
   },
@@ -21,19 +18,9 @@ export const manifest: chrome.runtime.ManifestV3 = {
   permissions: ["nativeMessaging", "tabs", "history", "bookmarks", "downloads"],
   host_permissions: ["*://*/*"],
   icons: {
-    16: "icons/16.png",
-    19: "icons/19.png",
-    32: "icons/32.png",
-    38: "icons/38.png",
     48: "icons/48.png",
-    64: "icons/64.png",
-    96: "icons/96.png",
     128: "icons/128.png",
     256: "icons/256.png",
     512: "icons/512.png",
-  },
-  options_ui: {
-    page: "src/entries/options/index.html",
-    open_in_tab: false,
   },
 };
