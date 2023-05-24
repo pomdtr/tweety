b26c55ae5ea5b29a81fa55a6c1046a881681563a
pomdtr <achille@deta.sh>
2023-05-24 10:36:53 +0200

    fix entrypoint generation

--- cmd/init.go
+++ cmd/init.go
@@ -22,8 +22,9 @@ var (
 )
 
 var (
-	manifestTmpl  = template.Must(template.New("manifest").Parse(string(manifest)))
-	manifestPaths = map[string]string{
+	manifestTmpl   = template.Must(template.New("manifest").Parse(string(manifest)))
+	entrypointTmpl = template.Must(template.New("entrypoint").Parse(string(entrypoint)))
+	manifestPaths  = map[string]string{
 		"chrome":  filepath.Join(xdg.DataHome, "Google", "Chrome", "NativeMessagingHosts", manifestName),
 		"edge":    filepath.Join(xdg.DataHome, "microsoft", "edge", "NativeMessagingHosts", manifestName),
 		"brave":   filepath.Join(xdg.DataHome, "BraveSoftware", "Brave-Browser", "NativeMessagingHosts", manifestName),
@@ -57,22 +58,33 @@ func NewCmdInit() *cobra.Command {
 				return fmt.Errorf("unable to create manifest directory: %w", err)
 			}
 
-			writer := bytes.Buffer{}
-			if err := manifestTmpl.Execute(&writer, map[string]string{
+			manifestBuffer := bytes.Buffer{}
+			if err := manifestTmpl.Execute(&manifestBuffer, map[string]string{
 				"homeDir":     homeDir,
 				"extensionID": flags.ExtensionID,
 			}); err != nil {
 				return fmt.Errorf("unable to execute manifest template: %w", err)
 			}
 
-			if err := os.WriteFile(manifestPath, writer.Bytes(), 0644); err != nil {
+			if err := os.WriteFile(manifestPath, manifestBuffer.Bytes(), 0644); err != nil {
 				return fmt.Errorf("unable to write manifest file: %w", err)
 			}
 			cmd.Printf("Manifest file written successfully\n")
 
+			if err := os.MkdirAll(filepath.Join(homeDir, ".local", "bin"), 0755); err != nil {
+				return fmt.Errorf("unable to create entrypoint directory: %w", err)
+			}
+
+			entrypointBuffer := bytes.Buffer{}
+			if err := entrypointTmpl.Execute(&entrypointBuffer, map[string]string{
+				"popcornBin": os.Args[0],
+			}); err != nil {
+				return fmt.Errorf("unable to execute entrypoint template: %w", err)
+			}
+
 			entrypointPath := filepath.Join(homeDir, ".local", "bin", "popcorn.sh")
 			cmd.Printf("Writing entrypoint file to %s\n", entrypointPath)
-			if err := os.WriteFile(entrypointPath, []byte(entrypoint), 0755); err != nil {
+			if err := os.WriteFile(entrypointPath, entrypointBuffer.Bytes(), 0755); err != nil {
 				return fmt.Errorf("unable to write entrypoint file: %w", err)
 			}
 			cmd.Printf("Entrypoint file written successfully\n")
--- cmd/popcorn.sh
+++ cmd/popcorn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/Users/achille/go/bin/popcorn server
+{{ .popcornBin }} server
