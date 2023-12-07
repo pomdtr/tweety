0a65849190680a291cea8be419c10d8bc2892b74
pomdtr <achille.lacoin@gmail.com>
2023-12-07 18:24:11 +0100

    fix favicons

--- server.go
+++ server.go
@@ -32,7 +32,7 @@ func NewHandler() (http.Handler, error) {
 			w.Header().Set("X-Content-Type-Options", "nosniff")
 			w.Header().Set("X-XSS-Protection", "1; mode=block")
 			w.Header().Set("Referrer-Policy", "same-origin")
-			w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self';")
+			w.Header().Set("Content-Security-Policy", "script-src 'self';")
 			next.ServeHTTP(w, r)
 		})
 	})
