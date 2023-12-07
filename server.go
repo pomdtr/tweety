aac498a6e5574fe8083771fabc82f87557b17472
pomdtr <achille.lacoin@gmail.com>
2023-12-07 12:20:19 +0100

    add appropriate x-frame options

--- server.go
+++ server.go
@@ -29,6 +29,11 @@ func NewHandler() (http.Handler, error) {
 	r.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Private-Network", "true")
+			w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.Header().Set("X-XSS-Protection", "1; mode=block")
+			w.Header().Set("Referrer-Policy", "same-origin")
+			w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self';")
 			next.ServeHTTP(w, r)
 		})
 	})
