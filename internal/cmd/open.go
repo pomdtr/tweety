84642635006a2118da337f60d92049fcacc861e0
pomdtr <achille.lacoin@gmail.com>
2025-06-03 14:06:24 +0200

    fix: use absolute path for file URLs in open command

--- internal/cmd/open.go
+++ internal/cmd/open.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"path/filepath"
 
 	"github.com/pomdtr/tweety/internal/jsonrpc"
 	"github.com/spf13/cobra"
@@ -16,9 +17,14 @@ func NewCmdOpen() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var options map[string]string
 			if _, err := os.Stat(args[0]); err == nil {
+				fp, err := filepath.Abs(args[0])
+				if err != nil {
+					return fmt.Errorf("failed to get absolute path: %w", err)
+				}
+
 				url := url.URL{
 					Scheme: "file",
-					Path:   args[0],
+					Path:   fp,
 				}
 				options = map[string]string{
 					"url": url.String(),
