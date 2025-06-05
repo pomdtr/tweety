bc3f0de1e356b1c2058f27711e6d9cf89c48d53f
pomdtr <achille.lacoin@gmail.com>
2025-06-05 11:25:32 +0200

    fix default shortcut on firefox

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     outDir: "dist",
-    manifest: ({ browser }) => ({
+    manifest: ({ browser, manifestVersion }) => ({
         key: browser == "chrome" ? "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlT6IZrhsX6Y4GNar3eVY7gDmH1mujxci6QeeaiFMvz4TrJ1VSCy4eCBC3XLBnmN4Evi//pgG8XeW9Ock4aZhLr/zQboQ8uuqv2V/MvHYRZqirghYnVIPZ8FaMiYIwCPpG/dB+PsYlpsxtb0vDEfa0RYt7uUAERBhOCIX/j47TdiuUpvARKZaoPSFZCUdgq7n4XcEv0sZtjhuXR2tD7rgqmZgu6FGlO4CvshdWcXHMmiZWssfYcHUGeJP/Zbcs0tqwk7LstT80zGtVSUu1ey7CQxKZTAaNZVglyye2rSECR52UzTIeHI92gZjsFl7tENs3Hs+lY3ReVJGhhF3ksTn0QIDAQAB" : undefined,
         name: "Tweety",
         description: "An integrated terminal for your browser",
@@ -21,19 +21,16 @@ export default defineConfig({
         host_permissions: [
             "<all_urls>"
         ],
-        commands: {
-            _execute_action: {
+        commands: manifestVersion == 3 ? {
+        } : {
+            _execute_browser_action: {
                 suggested_key: {
-                    default: "Ctrl+J",
-                    mac: "Command+J",
+                    default: "Ctrl+Shift+J",
+                    mac: "Command+Shift+J",
                 }
             },
             openInNewTab: {
-                description: "Open in new tab",
-                suggested_key: {
-                    default: "Ctrl+Shift+T",
-                    mac: "Command+Shift+T",
-                }
+                description: "Open in new tab"
             },
             openInNewWindow: {
                 description: "Open in new window",
