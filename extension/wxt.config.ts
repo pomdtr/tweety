2a355b73bd3d81e34dee181f58cdaf2fd2084dbd
pomdtr <achille.lacoin@gmail.com>
2025-06-05 13:09:44 +0200

    bundle extensions in the zip

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     outDir: "dist",
-    manifest: ({ browser, manifestVersion }) => ({
+    manifest: ({ browser }) => ({
         key: browser == "chrome" ? "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlT6IZrhsX6Y4GNar3eVY7gDmH1mujxci6QeeaiFMvz4TrJ1VSCy4eCBC3XLBnmN4Evi//pgG8XeW9Ock4aZhLr/zQboQ8uuqv2V/MvHYRZqirghYnVIPZ8FaMiYIwCPpG/dB+PsYlpsxtb0vDEfa0RYt7uUAERBhOCIX/j47TdiuUpvARKZaoPSFZCUdgq7n4XcEv0sZtjhuXR2tD7rgqmZgu6FGlO4CvshdWcXHMmiZWssfYcHUGeJP/Zbcs0tqwk7LstT80zGtVSUu1ey7CQxKZTAaNZVglyye2rSECR52UzTIeHI92gZjsFl7tENs3Hs+lY3ReVJGhhF3ksTn0QIDAQAB" : undefined,
         name: "Tweety",
         description: "An integrated terminal for your browser",
-        version: "0.1.0",
+        version: process.env.MANIFEST_VERSION || "0.0.0",
         permissions: [
             "tabs",
             "nativeMessaging",
